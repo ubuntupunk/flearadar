0230d90090a0a79d5749f0534494a0000a3721c8
David Robert Lewis <ubuntupunk@gmail.com>
2025-03-06 03:17:36 -0500

    fix: css articles

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,58 +1,56 @@
-// pages/articles/[slug].js
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import Markdown from 'markdown-to-jsx';
 
-const ArticlePage = ({ content, frontmatter }) => {
-  return (
-    <div className="article-container">
-    <h1 className="article-title">{frontmatter.title}</h1>
-    <p className="article-description">{frontmatter.description}</p>
-    {/* <Markdown className="article-content">{content}</Markdown> */}
-    <Markdown
-  options={{
-    overrides: {
-      h1: {
-        component: (props) => <h1 className="article-title" {...props} />,
-      },
-      p: {
-        component: (props) => <p className="article-content" {...props} />,
-      },
-      // Add more overrides as needed
-    },
-  }}
->
-  {content}
-</Markdown>
-  
-  </div>
-  );
-};
-
-export async function getStaticPaths() {
+export async function generateStaticParams() {
   const articlesDirectory = path.join(process.cwd(), 'articles');
   const filenames = fs.readdirSync(articlesDirectory);
 
-  const paths = filenames.map((filename) => ({
-    params: { slug: filename.replace(/\.md$/, '') }, // Remove .md extension for slug
+  return filenames.map((filename) => ({
+    slug: filename.replace(/\.md$/, ''), // Remove .md extension for slug
   }));
-
-  return { paths, fallback: false };
 }
 
-export async function getStaticProps({ params }) {
+async function getArticle(slug: string) {
   const articlesDirectory = path.join(process.cwd(), 'articles');
-  const filePath = path.join(articlesDirectory, `${params.slug}.md`);
+  const filePath = path.join(articlesDirectory, `${slug}.md`);
   const fileContents = fs.readFileSync(filePath, 'utf8');
   const { data, content } = matter(fileContents);
 
   return {
-    props: {
-      frontmatter: data,
-      content,
-    },
+    frontmatter: data,
+    content,
   };
 }
 
-export default ArticlePage;
+interface Params {
+  slug: string;
+}
+
+export default async function ArticlePage({ params }: { params: Params }) {
+  const { slug } = params;
+  const article = await getArticle(slug);
+
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-4">{article.frontmatter.title}</h1>
+      <p className="text-gray-700 leading-relaxed">{article.frontmatter.description}</p>
+      <Markdown
+        options={{
+          overrides: {
+            h1: {
+              component: (props) => <h1 className="text-2xl font-bold mb-2" {...props} />,
+            },
+            p: {
+              component: (props) => <p className="text-gray-700 leading-relaxed" {...props} />,
+            },
+            // Add more overrides as needed
+          },
+        }}
+      >
+        {article.content}
+      </Markdown>
+    </div>
+  );
+}
